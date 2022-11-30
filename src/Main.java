9756916b2cd06e46b97c7a927c0462364bb36395
Veryval <veryval@mail.ru>
2022-12-01 01:16:16 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,54 @@
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    static final int SIZE = 1000;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
+        Runnable generateText = () -> {
 
-
-        ExecutorService threadPool = Executors.newFixedThreadPool(1000);
-
-        final Callable<String> textStrings = () -> {
-
-            String text =generateRoute("RLRFR", 100);
+            String text = generateRoute("RLRFR", 100);
             int letterR = countRight(text);
             synchronized (sizeToFreq) {
-                if (sizeToFreq.containsKey(letterR)){
-                    sizeToFreq.put(letterR, sizeToFreq.get(letterR)+1);
-                }else sizeToFreq.put(letterR, 1);
-
+                if (sizeToFreq.containsKey(letterR)) {
+                    sizeToFreq.put(letterR, sizeToFreq.get(letterR) + 1);
+                } else {
+                    sizeToFreq.put(letterR, 1);
+                }
+                sizeToFreq.notify();
+                //System.out.println("Завершен первый поток ");
             }
-            return text.toString();
+
         };
 
-        for (int i=0; i<=1000; i++) {
-            threadPool.submit(textStrings);
-        }
 
-        //System.out.println(sizeToFreq.entrySet());
-       printMap(sizeToFreq);
+        Runnable countMapa = () -> {
+
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                printMap(sizeToFreq);
+            }
+            //System.out.println("Завершен второй поток");
+        };
+        Thread thread2 = new Thread(countMapa);
+        thread2.start();
+
+        for (int i = 0; i <= SIZE - 1; i++) {
+            Thread thread1 = new Thread(generateText);
+            thread1.start();
+            thread1.join();
+        }
+        thread2.interrupt();
+
     }
 
 
@@ -46,10 +61,10 @@ public class Main {
         return route.toString();
     }
 
-    public static int countRight (String string){
+    public static int countRight(String string) {
         char[] stringMass = string.toCharArray();
         int count = 0;
-        for (char r : stringMass){
+        for (char r : stringMass) {
             if (r == 'R') {
                 count++;
             }
@@ -57,21 +72,25 @@ public class Main {
         return count;
     }
 
-    public static void printMap (Map<Integer,Integer> map) {
+    public static void printMap(Map<Integer, Integer> map) {
         Map<Integer, Integer> sortedMap = map.entrySet().stream()
                 .sorted(Comparator.comparingInt(e -> -e.getValue()))
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         Map.Entry::getValue,
-                        (a, b) -> { throw new AssertionError(); },
+                        (a, b) -> {
+                            throw new AssertionError();
+                        },
                         LinkedHashMap::new
                 ));
 
+        Map.Entry<Integer, Integer> mapEntry = map.entrySet().iterator().next();
+        Integer key = mapEntry.getKey();
+        Integer value = mapEntry.getValue();
 
-        System.out.print("Самое частое количество повторений" );
-
-        for(Map.Entry<Integer,Integer>ite : sortedMap.entrySet())
-            System.out.print("- "+ ite.getKey()+"("+ ite.getValue()+" раз)"+"\n");
+        System.out.print("Самое частое количество повторений -" + key + " (" + value + "  раз)" + "\n");
+        //for (Map.Entry<Integer, Integer> ite : sortedMap.entrySet())
+        //    System.out.print("- " + ite.getKey() + "(" + ite.getValue() + " раз)" + "\n");
 
     }
 
